e50b1778cb7eb04285a26ec29911d44651cb806a
optimizershivam <itsurshivam9@gmail.com>
2022-05-26 00:43:05 +0530

    use effect 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo';
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
